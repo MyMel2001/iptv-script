6524979a4bcb49b9a3aae437ca9f3a6743d4e7b7
MyMel2001 Holder <163201376+mymel2001-holder@users.noreply.github.com>
2025-11-14 18:53:00 -0600

    Update generate.sh

--- generate.sh
+++ generate.sh
@@ -1,8 +1,16 @@
 echo "#EXTM3U" > list.m3u
+echo " " >> list.m3u
 curl -s "https://raw.githubusercontent.com/Free-TV/IPTV/refs/heads/master/playlists/playlist_usa.m3u8" | sed 's/#extm3u//gi' | sed 's/extm3u//gi' >> list.m3u
+echo " " >> list.m3u
 curl -s "https://raw.githubusercontent.com/iptv-org/iptv/refs/heads/master/streams/us_xumo.m3u" | sed 's/#extm3u//gi' | sed 's/extm3u//gi' >> list.m3u
+echo " " >> list.m3u
 curl -s "https://raw.githubusercontent.com/iptv-org/iptv/refs/heads/master/streams/us_pluto.m3u" | sed 's/#extm3u//gi' | sed 's/extm3u//gi' >> list.m3u
+echo " " >> list.m3u
 curl -s "https://raw.githubusercontent.com/iptv-org/iptv/refs/heads/master/streams/us_local.m3u" | sed 's/#extm3u//gi' | sed 's/extm3u//gi' >> list.m3u
+echo " " >> list.m3u
 curl -s "https://raw.githubusercontent.com/iptv-org/iptv/refs/heads/master/streams/us_tubi.m3u" | sed 's/#extm3u//gi' | sed 's/extm3u//gi' >> list.m3u
+echo " " >> list.m3u
 curl -s "https://raw.githubusercontent.com/iptv-org/iptv/refs/heads/master/streams/us_stirr.m3u" | sed 's/#extm3u//gi' | sed 's/extm3u//gi' >> list.m3u
+echo " " >> list.m3u
 curl -s "https://raw.githubusercontent.com/iptv-org/iptv/refs/heads/master/streams/us_firetv.m3u" | sed 's/#extm3u//gi' | sed 's/extm3u//gi' >> list.m3u
+echo " " >> list.m3u
